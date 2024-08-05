0c3aa45c998a98f1c8785a6c76038ab606a4257a
diana <diana7170368@gmail.com>
2024-08-04 20:04:24 -0500

    dandole al frontend 04/08/24

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <nav>
+          <a href="">
+            <i></i>
+          </a>
+        </nav>
+      </header>
+      <aside></aside>
+      <main>
+        <section>
+          <article>
+            <form action="">
+              <label htmlFor=""> Nombres </label>
+              <input type="text" placeholder="Ingrese sus nombre" />
+              <label htmlFor="">  Apellido </label>
+              <input type="text" placeholder="Ingrese sus apellidos" />
+              <label htmlFor=""> Edad </label>
+              <input type="number" placeholder="Ingrese su edad" />
+              <button>Enviar</button>
+            </form>
+            </article>
+        </section>
+      </main>
+      <asiden></asiden>
+      <footer></footer>
     </>
-  )
+  );
 }
-
-export default App
