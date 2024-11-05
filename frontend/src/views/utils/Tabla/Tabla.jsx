d2cba3ee5da2217e2170b1cfaaff0298306d9d3b
diana <diana7170368@gmail.com>
2024-11-05 00:11:23 -0500

    subiendo estilos

--- frontend/src/views/utils/Tabla/Tabla.jsx
+++ frontend/src/views/utils/Tabla/Tabla.jsx
@@ -3,20 +3,20 @@ import "./Tabla.css";
 import * as XLSX from "xlsx";
 import { SiMicrosoftexcel } from "react-icons/si";
 import { BotonAcciones } from "../BotonAcciones/BotonAcciones";
-import {FaPlus} from "react-icons/fa"; 
-import {Contadores} from '../Contadores/Contadores'
+import { FaPlus } from "react-icons/fa";
+import { Contadores } from "../Contadores/Contadores";
 import { useNavigate } from "react-router-dom";
-export const Tabla = ({ datos, contadores, titulo}) => {
+export const Tabla = ({ datos, contadores, titulo }) => {
   const [paginaActual, setPaginaActual] = useState(1);
   const [filtros, setFiltros] = useState({});
   const [aplicarFiltro, setAplicarFiltro] = useState(false);
   const filasPorPagina = 5;
   const navigate = useNavigate();
-  if (!datos || datos.length === 0) {
-    return <p>No hay datos disponibles para mostrar.</p>;
-  }
-
+  let cabeceras = [];
+if(datos && datos.length > 0){
   const cabeceras = Object.keys(datos[0]);
+}
+
 
   // Cambiar el valor de los filtros en el estado
   const handleFilterChange = (e, columna) => {
@@ -53,7 +53,10 @@ export const Tabla = ({ datos, contadores, titulo}) => {
 
   const indiceUltimaFila = paginaActual * filasPorPagina;
   const indicePrimeraFila = indiceUltimaFila - filasPorPagina;
-  const filasActuales = datosFiltrados.slice(indicePrimeraFila, indiceUltimaFila);
+  const filasActuales = datosFiltrados.slice(
+    indicePrimeraFila,
+    indiceUltimaFila
+  );
   const numeroTotalPaginas = Math.ceil(datosFiltrados.length / filasPorPagina);
 
   const cambiarPagina = (numeroPagina) => {
@@ -69,12 +72,17 @@ export const Tabla = ({ datos, contadores, titulo}) => {
     const worksheet = XLSX.utils.json_to_sheet(datosFiltrados);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Registros");
-    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-    const blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+    const blob = new Blob([excelBuffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
     const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
-    link.download = 'registros.xlsx'; // Nombre del archivo
+    link.download = "registros.xlsx"; // Nombre del archivo
     link.click();
     URL.revokeObjectURL(url);
   };
@@ -86,15 +94,18 @@ export const Tabla = ({ datos, contadores, titulo}) => {
         <input
           type="number"
           placeholder={`Filtrar por ${columna}`}
-          value={filtros[columna] || ''}
+          value={filtros[columna] || ""}
           onChange={(e) => handleFilterChange(e, columna)}
         />
       );
-    } else if (typeof sampleValue === "string" && sampleValue.match(/^\d{4}-\d{2}-\d{2}/)) {
+    } else if (
+      typeof sampleValue === "string" &&
+      sampleValue.match(/^\d{4}-\d{2}-\d{2}/)
+    ) {
       return (
         <input
           type="date"
-          value={filtros[columna] || ''}
+          value={filtros[columna] || ""}
           onChange={(e) => handleFilterChange(e, columna)}
         />
       );
@@ -103,7 +114,7 @@ export const Tabla = ({ datos, contadores, titulo}) => {
         <input
           type="text"
           placeholder={`Filtrar por ${columna}`}
-          value={filtros[columna] || ''}
+          value={filtros[columna] || ""}
           onChange={(e) => handleFilterChange(e, columna)}
         />
       );
@@ -112,7 +123,7 @@ export const Tabla = ({ datos, contadores, titulo}) => {
       <input
         type="text"
         placeholder={`Filtrar por ${columna}`}
-        value={filtros[columna] || ''}
+        value={filtros[columna] || ""}
         onChange={(e) => handleFilterChange(e, columna)}
       />
     );
@@ -120,70 +131,89 @@ export const Tabla = ({ datos, contadores, titulo}) => {
 
   return (
     <>
-    <h1 className="">{titulo}</h1>
-      <div className="cont-tittle-descargar">
-        
-        <Contadores  contadores = {contadores} />
-        <button onClick={handleDownload} className="btn-descargar">
-          <SiMicrosoftexcel className="excel-icon" /> Descargar Registros
+      {(datos || datos.length >0) && (
+  <div className="contenedor">
+    <h1>{titulo}</h1>
+    <div className="filtros">
+      {cabeceras.map((columna, index) => (
+        <div key={index} className="filtro">
+          <label>{columna}</label>
+          {getInputField(columna)}
+        </div>
+      ))}
+      <div className="btn-filtros">
+        <button onClick={aplicarFiltros} className="btn">
+          Buscar
         </button>
-        <button 
-        className="crear-beneficiario"
-        onClick={() => navigate('/formulario')} // Cambia '/formulario' a la ruta que tengas configurada para el formulario
-      >
-        <FaPlus size={20} color="#ffffff" /> Crear beneficiario
-      </button>
+        <button onClick={borrarFiltros} className="btn">
+          Reestablecer
+        </button>
       </div>
-     
-     
-      <div className="filtros">
-        {cabeceras.map((columna, index) => (
-          <div key={index} className="filtro">
-            <label>{columna}</label>
-            {getInputField(columna)}
-          </div>
-        ))}
-        <button onClick={aplicarFiltros} className="btn">Buscar</button>
-        <button onClick={borrarFiltros} className="btn">Borrar Filtros</button>
-      </div>
+    </div>
+  </div>
+)}
 
-      <table className="tabla">
-        
+        <div className="cont-crear-descargar">
+          <Contadores contadores={contadores} />
+          <button onClick={handleDownload} className="btn-descargar">
+            <SiMicrosoftexcel className="excel-icon" /> Descargar
+          </button>
+          <button
+            className="crear-beneficiario"
+            onClick={() => navigate("/formulario")} // Cambia '/formulario' a la ruta que tengas configurada para el formulario
+          >
+            <FaPlus className="FaPlus-icon" size={20} color="#ffffff" /> Crear
+          </button>
+        </div>
 
-
-        <thead>
-          <tr>
-            {cabeceras.map((cabecera, index) => (
-              <th key={index}>{cabecera}</th>
-            ))}
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filasActuales.map((fila, index) => (
-            <tr key={index}>
-              {cabeceras.map((cabecera) => (
-                <td key={cabecera}>{fila[cabecera]}</td>
-              ))}
-              <td>
-                <BotonAcciones />
-              </td>
-            </tr>
+        {!datos || datos.length === 0 ? (
+  <p>No hay datos disponibles para mostrar.</p>
+) : (
+  <>
+    <table className="tabla">
+      <thead>
+        <tr>
+          {cabeceras.map((cabecera, index) => (
+            <th key={index}>{cabecera}</th>
           ))}
-        </tbody>
-      </table>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filasActuales.map((fila, index) => (
+          <tr key={index}>
+            {cabeceras.map((cabecera) => (
+              <td key={cabecera}>{fila[cabecera]}</td>
+            ))}
+            <td>
+              <BotonAcciones />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
 
-      <div className="paginacion">
-        <button onClick={() => cambiarPagina(paginaActual - 1)} disabled={paginaActual === 1}>
-          Anterior
-        </button>
-        <span>
-          Página {paginaActual} de {numeroTotalPaginas}
-        </span>
-        <button onClick={() => cambiarPagina(paginaActual + 1)} disabled={paginaActual === numeroTotalPaginas}>
-          Siguiente
-        </button>
-      </div>
+    <div className="paginacion">
+      <button
+        onClick={() => cambiarPagina(paginaActual - 1)}
+        disabled={paginaActual === 1}
+      >
+        Anterior
+      </button>
+      <span>
+        Página {paginaActual} de {numeroTotalPaginas}
+      </span>
+      <button
+        onClick={() => cambiarPagina(paginaActual + 1)}
+        disabled={paginaActual === numeroTotalPaginas}
+      >
+        Siguiente
+      </button>
+    </div>
+  </>
+)}
+
+      
     </>
   );
 };
